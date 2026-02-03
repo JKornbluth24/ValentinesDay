152418288b5c2678c16584ca86c0293c6dd2b9ef
JKornbluth24 <jakornbluth@gmail.com>
2026-02-02 21:58:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const yesButton = document.getElementById("yesBtn");
 const noButton = document.getElementById("noBtn");
 
+// YES BUTTON
 yesButton.addEventListener("click", () => {
     document.body.innerHTML = "<h1>YAY!!! 💖💖💖</h1><p>I knew you'd say yes 😘</p>";
 });
 
+// NO BUTTON RUNS AWAY
 noButton.addEventListener("mouseover", () => {
     const x = Math.random() * (window.innerWidth - noButton.offsetWidth);
     const y = Math.random() * (window.innerHeight - noButton.offsetHeight);
@@ -13,3 +15,22 @@ noButton.addEventListener("mouseover", () => {
     noButton.style.left = x + "px";
     noButton.style.top = y + "px";
 });
+
+// CREATE CHICKENS
+function createChicken() {
+    const chicken = document.createElement("div");
+    chicken.innerText = "🐔";
+    chicken.classList.add("chicken");
+
+    chicken.style.top = Math.random() * window.innerHeight + "px";
+    chicken.style.animationDuration = (Math.random() * 3 + 4) + "s";
+
+    document.body.appendChild(chicken);
+
+    setTimeout(() => {
+        chicken.remove();
+    }, 8000);
+}
+
+// SPAWN CHICKENS CONTINUOUSLY
+setInterval(createChicken, 700);
