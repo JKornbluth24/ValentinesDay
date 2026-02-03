4e053a528d987210aa89fd109b578d1f336a4331
JKornbluth24 <jakornbluth@gmail.com>
2026-02-02 21:54:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const yesButton = document.getElementById("yesBtn");
+const noButton = document.getElementById("noBtn");
+
+yesButton.addEventListener("click", () => {
+    document.body.innerHTML = "<h1>YAY!!! 💖💖💖</h1><p>I knew you'd say yes 😘</p>";
+});
+
+noButton.addEventListener("mouseover", () => {
+    const x = Math.random() * (window.innerWidth - noButton.offsetWidth);
+    const y = Math.random() * (window.innerHeight - noButton.offsetHeight);
+
+    noButton.style.position = "absolute";
+    noButton.style.left = x + "px";
+    noButton.style.top = y + "px";
+});
